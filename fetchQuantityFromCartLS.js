15de6a01c3519467329afc09b7c4efe75b1c8e69
KunalSharma2003 <132732207+KunalSharma2003@users.noreply.github.com>
2024-04-15 23:11:36 +0530

    Update fetchQuantityFromCartLS.js

--- fetchQuantityFromCartLS.js
+++ fetchQuantityFromCartLS.js
@@ -6,7 +6,7 @@ export const fetchQuantityFromCartLS = (id, price) => {
   let quantity = 1;
   if (existingProduct) {
     quantity = existingProduct.quantity;
-    price = Math.floor(existingProduct.price * 100) / 100;
+    price = Math.round(existingProduct.price * 100) / 100;
   }
   return { quantity, price };
 };
